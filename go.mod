5309cc4864a84c0a6f5c9b6ded8e97c0e4638770
陈家辉 <chenjiahui@haolianluo.com>
2020-07-07 13:44:37 +0800

    添加配置文件

--- go.mod
+++ go.mod
@@ -4,5 +4,7 @@ go 1.12
 
 require (
 	github.com/detectiveHLH/go-backend-starter v0.0.0-20191202071258-b02ff283dd2b
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.6.3
+	gopkg.in/ini.v1 v1.57.0
 )
---
+++ util/jwt.go
@@ -0,0 +1,46 @@
+package util
+
+import (
+	"github.com/dgrijalva/jwt-go"
+	"time"
+)
+
+var jwtSecret = []byte(setting.AppSetting.jwtSecret)
+
+type Claims struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+	jwt.StandardClaims
+}
+
+//建立token
+func GenerateToken(username, password string) (string, error) {
+	nowTime := time.Now()
+	exprieTime := nowTime.Add(3 * time.Hour)
+	claims := Claims{
+		username,
+		password,
+		jwt.StandardClaims {
+			ExpiresAt : expireTime.Unix(),
+			Issuer : "go-backend-starter"
+		},
+	}
+	tokenClaims := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	token, err := tokenClaims.SignedString(jwtSecret)
+
+	return token, err
+}
+
+//解析token
+func ParseToken(token String) (*Claims, error) {
+	tokenClaims, err := jwt.ParseWithClaims(token, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+		return jwtSecret, nil
+	})
+	if tokenClaims != nil {
+		if claims, ok := tokenClaims.Claims.(*Claims); ok && tokenClaims.Valid {
+			return claims, nil
+		}
+	}
+
+	return nil, err
+}
