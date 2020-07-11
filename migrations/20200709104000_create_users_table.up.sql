be0da79ac2e135d5c8a0c8118f4360c8a9059430
陈家辉 <chenjiahui@haolianluo.com>
2020-07-11 15:54:06 +0800

    添加所有sql文件

--- migrations/20200709104000_create_users_table.up.sql
+++ migrations/20200709104000_create_users_table.up.sql
@@ -2,5 +2,6 @@ CREATE TABLE IF NOT EXISTS party_users(
    id INT,
    name VARCHAR(100) NOT NULL,
    password VARCHAR(40) NOT NULL,
+   unionid INT(11) NOT NULL COMMENT '员工在当前开发者企业账号范围内的唯一标识',
    PRIMARY KEY ( id )
 );
